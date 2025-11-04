b5381815a9973cbc1d877ac18d70c881127c6317
neh-conve <165389299+neh-conve@users.noreply.github.com>
2025-11-04 08:51:44 +0530

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Student_CTS">
+      <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Index />} /> */}
           <Route path="/" element={<StudentLandingPage />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Student_CTS/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  server: { host: true },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
